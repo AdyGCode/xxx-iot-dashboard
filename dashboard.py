c9691db39bc3043073d8ca099d645c5edcd26121
Ady G [TAFE] <adrian.gould@nmtafe.wa.edu.au>
2021-06-02 07:24:22 +0800

    Added 'todo list' to the index page

--- dashboard.py
+++ dashboard.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
-app = Flask(__name__)
+MyDashboardApp = Flask(__name__)
 
 
-@app.route('/')
+@MyDashboardApp.route("/")  # http://localhost:5000/
 def hello_world():
-    return 'Hello World!'
+    return render_template('index.html',
+                           page="home")
+
+
+@MyDashboardApp.route("/clock")
+def clock():
+    return render_template('clock.html',
+                           page='clock')
+
+
+@MyDashboardApp.route("/about")
+def about_app():
+    return render_template('about.html',
+                           page="about")
 
 
 if __name__ == '__main__':
-    app.run()
+    MyDashboardApp.run()
