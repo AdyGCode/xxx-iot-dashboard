02d5e91be3fb5d95eca0583d723cc8db1d0b7f4f
Ady G [TAFE] <adrian.gould@nmtafe.wa.edu.au>
2021-06-01 23:17:01 +0800

    Base code

---
+++ dashboard-debug.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+FLASK_APP=dashboard.py FLASK_ENV=development python3 -m flask run --port=5000
---
+++ dashboard-production.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+FLASK_APP=dashbaord.py FLASK_ENV=production python3 -m flask run --port=5000
